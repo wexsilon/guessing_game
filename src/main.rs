3ea129aeeaf64b4c929b20d29a7e1a7996fe5734
wexsilon <wexsilon2001@protonmail.com>
2023-09-18 02:30:56 +0330

    add simple text input code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("-*- Guessing Game -*-");
+    print!("=> ");
+
+    io::stdout().flush().expect("Error! flush");
+
+    let mut text_input = String::new();
+
+    io::stdin().read_line(&mut text_input).expect("Error! read_line");
+
+    
+
 }
