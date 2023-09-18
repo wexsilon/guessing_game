adbac5401ec58e22b6a7335a8a4ff37c6a0dbfad
wexsilon <wexsilon2001@protonmail.com>
2023-09-18 13:17:11 +0330

    complete guess game

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
-use std::io::{self, Write};
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("-*- Guessing Game -*-");
-    print!("=> ");
-
-    io::stdout().flush().expect("Error! flush");
-
-    let mut text_input = String::new();
-
-    io::stdin().read_line(&mut text_input).expect("Error! read_line");
-
     
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    
+    loop {
+        println!("=> ");
+        let mut guess_number = String::new();
+        io::stdin().read_line(&mut guess_number).expect("Error! read_line");
+        let guess_number: u32 = match guess_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+            Ordering::Greater => println!("Too big!")
+        }
+    }
+    
 }
